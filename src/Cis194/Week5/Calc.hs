170a04514b82e3dd2e9fb92699f4f7e94bf9928c
Andrew Bennett <potatosaladx@gmail.com>
2014-06-14 11:31:56 -0600

    evalStr for Exercise 2 from Week 5.

--- src/Cis194/Week5/Calc.hs
+++ src/Cis194/Week5/Calc.hs
@@ -1,8 +1,15 @@
 module Cis194.Week5.Calc where
 import Cis194.Week5.ExprT
+import Cis194.Week5.Parser
 
 -- Exercise 1
 eval :: ExprT -> Integer
 eval (Lit x)   = x
 eval (Add x y) = (+) (eval x) (eval y)
 eval (Mul x y) = (*) (eval x) (eval y)
+
+-- Exercise 2
+evalStr :: String -> Maybe Integer
+evalStr string = case (parseExp Lit Add Mul string) of
+  Nothing -> Nothing
+  (Just expression) -> Just (eval expression)
--- test/Cis194/Week5/CalcSpec.hs
+++ test/Cis194/Week5/CalcSpec.hs
@@ -13,13 +13,13 @@ spec = do
     it "should add and multiply numbers" $ do
       eval (Mul (Add (Lit 2) (Lit 3)) (Lit 4)) `shouldBe` 20
 
-  --describe "Calculator - evalStr" $ do
-  --  it "should evaluate well formed strings" $ do
-  --    evalStr "(2+3)*4" `shouldBe` Just 20
-  --  it "evals the multiplication operator before addition" $ do
-  --    evalStr "2+3*4" `shouldBe` Just 14
-  --  it "returns Nothing if the string is malformed" $ do
-  --    evalStr "2+3*" `shouldBe` Nothing
+  describe "Calculator - evalStr" $ do
+    it "should evaluate well formed strings" $ do
+      evalStr "(2+3)*4" `shouldBe` Just 20
+    it "evals the multiplication operator before addition" $ do
+      evalStr "2+3*4" `shouldBe` Just 14
+    it "returns Nothing if the string is malformed" $ do
+      evalStr "2+3*" `shouldBe` Nothing
 
   --describe "Expr ExprT" $ do
   --  it "should generate ExprT expression" $ do
